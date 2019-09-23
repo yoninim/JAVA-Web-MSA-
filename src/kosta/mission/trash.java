341bb76813a4cb05d0237b5143e2c5116f07deec
yoninim <51064759+yoninim@users.noreply.github.com>
2019-09-23 17:59:01 +0900

    cc

--- src/kosta/mission/trash.java
+++ src/kosta/mission/trash.java
@@ -1,5 +1,23 @@
 package kosta.mission;
 
-public class trash {
-	
-}
+	class ThreeArrays {
+	    static int arr1[];
+	    static {
+	        arr1 = new int[10];                   
+	        for (int cnt = 0; cnt < 10; cnt++)    
+	            arr1[cnt] = cnt + 1;
+	    }
+	    static int arr2[];
+	    static {
+	        arr2 = new int[10];                   
+	        for (int cnt = 0; cnt < 10; cnt++)    
+	            arr2[cnt] = (cnt + 1) * 10000;
+	    }
+	    static int arr3[];
+	    static {
+	        arr3 = new int[10];                   
+	        for (int cnt = 0; cnt < 10; cnt++)    
+	            arr3[cnt] = arr1[cnt] + arr2[cnt];
+	    }
+	}
+
